725d454c44a65582a27c451bd50b0a1ccd0278e7
wajih <wajihnahle@gmail.com>
2025-10-07 20:05:41 -0600

    FINALLY got playwright running again after vscode update removed some instilations.

---
+++ tests/admin.spec.ts
@@ -0,0 +1,4 @@
+// import { test, expect } from '@playwright/test' <- Replace this with the line below
+import { Page } from '@playwright/test';
+import { test, expect } from 'playwright-test-coverage';
+import { User, Role } from '../src/service/pizzaService';
